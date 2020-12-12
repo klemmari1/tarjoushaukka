054d0abf5df6c2e35383f779fefb3cf88a25ad38
Kristian Klemets <kristian.klemets@gmail.com>
2020-12-12 13:46:46 +0200

    Add endpoints to fetch and delete posts

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask_api import FlaskAPI
 
-from post_service import get_posts
+from post_service import fetch_posts, get_posts
+from posts import Post
 
 app = FlaskAPI(__name__)
 
@@ -13,6 +14,24 @@ def home():
 @app.route("/posts/", methods=["GET"])
 def posts_list():
     return get_posts()
+
+
+@app.route("/posts/fetch/", methods=["GET"])
+def posts_fetch():
+    fetch_posts()
+    return {"status": "ok"}
+
+
+@app.route("/posts/delete_old/", methods=["GET"])
+def posts_delete_old():
+    Post.delete_old_posts()
+    return {"status": "ok"}
+
+
+@app.route("/posts/delete_all/", methods=["GET"])
+def posts_delete_all():
+    Post.delete_all_posts()
+    return {"status": "ok"}
 
 
 if __name__ == "__main__":
