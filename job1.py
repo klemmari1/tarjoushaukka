3bf1a10da9dcf4bb4bde8916c31bec978a276203
Kristian Klemets <kristian.klemets@gmail.com>
2020-12-13 15:04:25 +0200

    Delete jobs

--- job1.py
+++
@@ -1,9 +0,0 @@
-from post_service import fetch_posts
-
-
-def fetch_posts() -> None:
-    fetch_posts()
-
-
-if __name__ == "__main__":
-    fetch_posts()
--- job2.py
+++
@@ -1,9 +0,0 @@
-from posts import Post
-
-
-def delete_old_posts() -> None:
-    Post.delete_old_posts()
-
-
-if __name__ == "__main__":
-    delete_old_posts()
